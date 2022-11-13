85210d74bbaeb14c2b6f596973f19415f773679e
Chris Pikul <chris@workmaple.com>
2022-11-13 06:07:50 -0800

    sketching out parts list

--- editor/src/components/PartsSection.tsx
+++ editor/src/components/PartsSection.tsx
@@ -6,7 +6,66 @@
  * 
  * Left-hand toolbox section for component selection
  */
+import type { FC } from 'react';
 import SectionHeader from './SectionHeader';
+
+interface PartTagProps {
+    id:string;
+    label:string;
+    description:string;
+    symbol?:string;
+    classification:string;
+    tags:string[];
+};
+
+const PartTag:FC<PartTagProps> = ({
+    id,
+    label,
+    description,
+    symbol,
+    classification,
+    tags,
+}) => (<div className='part-tag'>
+    <div className='part-tag-top'>
+        <h3>{ label }</h3>
+        <div className='img-placeholder' />
+    </div>
+    <p>{ description }</p>
+    <div className='part-tag-bottom'>
+        <i>{ classification }</i>
+    </div>
+</div>);
+
+const mockLibrary:PartTagProps[] = [
+    {
+        id: 'resistor',
+        label: 'Resistor',
+        description: 'Test component, here will be the description of the part for the catalog.',
+        classification: 'passive/resistor',
+        tags: [ 'passive', 'resistance', 'resistor' ],
+    },
+    {
+        id: 'capacitor',
+        label: 'Capacitor',
+        description: 'Test component, here will be the description of the part for the catalog.',
+        classification: 'passive/capacitor',
+        tags: [ 'passive', 'capacitance', 'capacitor', 'non-polar'],
+    },
+    {
+        id: 'capacitor-electrolytic',
+        label: 'Electrolytic Capacitor',
+        description: 'Test component, here will be the description of the part for the catalog.',
+        classification: 'passive/capacitor',
+        tags: [ 'passive', 'capacitance', 'capacitor', 'polar'],
+    },
+    {
+        id: 'diode',
+        label: 'Diode',
+        description: 'Test component, here will be the description of the part for the catalog.',
+        classification: 'active/diode',
+        tags: [ 'active', 'diode' ],
+    },
+];
 
 import './PartsSection.scss';
 export const PartsSection = () => {
@@ -15,17 +74,9 @@ export const PartsSection = () => {
         <div id='eme-toolbox-parts-content' className='plastic'>
             <div>Search</div>
             <ul id='eme-toolbox-parts-list'>
-                <li className='eme-toolbox-part'>
-                    Example Part
-                </li>
-
-                <li className='eme-toolbox-part'>
-                    Example Part
-                </li>
-
-                <li className='eme-toolbox-part'>
-                    Example Part
-                </li>
+                { mockLibrary.map(part => <li key={part.id} className='eme-toolbox-part'>
+                    <PartTag {...part} />
+                </li>)}
             </ul>
         </div>
     </section>;
